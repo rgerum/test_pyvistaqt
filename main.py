77575d112a126293e14f90b1b1a0a583f74e9076
rgerum <14153051+rgerum@users.noreply.github.com>
2023-03-31 10:25:48 -0400

    added intial data

---
+++ main.py
@@ -0,0 +1,13 @@
+from qtpy import QtCore, QtWidgets, QtGui
+from pyvistaqt import QtInteractor
+
+app = QtWidgets.QApplication([])
+window = QtWidgets.QWidget()
+print("ho")
+import pyvista as pv
+p = pv.Plotter(off_screen=True)
+p.add_mesh(pv.Cylinder(), color="tan", show_edges=True)
+p.show(screenshot="test.png")
+print("hu")
+
+QtInteractor(window, auto_update=False)
