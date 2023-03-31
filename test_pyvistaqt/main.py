5ce5ab26905d1adfcdc6a4a053d4a17966c378d5
rgerum <14153051+rgerum@users.noreply.github.com>
2023-03-31 10:37:06 -0400

    removed pyvista call from main.py

--- test_pyvistaqt/main.py
+++ test_pyvistaqt/main.py
@@ -1,13 +1,6 @@
-from qtpy import QtCore, QtWidgets, QtGui
+from qtpy import QtWidgets
 from pyvistaqt import QtInteractor
 
 app = QtWidgets.QApplication([])
 window = QtWidgets.QMainWindow()
-print("ho")
-import pyvista as pv
-p = pv.Plotter(off_screen=True)
-p.add_mesh(pv.Cylinder(), color="tan", show_edges=True)
-p.show(screenshot="test.png")
-print("hu")
-
-#QtInteractor(window, auto_update=False)
+QtInteractor(window, auto_update=False)
