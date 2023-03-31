f267ef1a7dd1128f195dbeebf0175a02b54685e9
rgerum <14153051+rgerum@users.noreply.github.com>
2023-03-31 10:57:20 -0400

    added main_without_pyvistaqt.py

---
+++ main_without_pyvistaqt.py
@@ -0,0 +1,9 @@
+print("start")
+import sys
+from qtpy import QtWidgets
+from pyvistaqt import QtInteractor
+
+app = QtWidgets.QApplication(sys.argv)
+window = QtWidgets.QMainWindow(None)
+#QtInteractor(window)
+print("success")
